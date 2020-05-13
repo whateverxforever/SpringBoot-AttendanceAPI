37627db24e617b9800dceeb7c06aac22279b465a
whateverxforever <40570503+whateverxforever@users.noreply.github.com>
2020-05-13 09:25:02 +0530

    added screenshots

--- demo/src/main/java/com/example/demo/dao/StudentDao.java
+++ demo/src/main/java/com/example/demo/dao/StudentDao.java
@@ -4,7 +4,6 @@ import com.example.demo.model.Student;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
 public interface StudentDao {
     int insertStudent(Student student);
